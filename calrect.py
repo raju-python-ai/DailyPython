f97b0856231e8d17aa516553a3ab393537bed04b
Rajasekar Surendran <rajasekar012004@gmail.com>
2025-02-10 16:11:05 +0530

    Update calrect.py

--- calrect.py
+++ calrect.py
@@ -6,4 +6,4 @@ length = int(input("Enter the length of the rectangle: "))
 width = int(input("Enter the width of the rectangle: "))
 
 area = calculate_area(length, width)
-print(f"The area of the rectangle is: {area}")
+print("The area of the rectangle is:)
