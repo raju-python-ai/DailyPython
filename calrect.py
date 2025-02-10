47c9ea76f6c4ea870d2637182d3bf89216ed4763
raju-python-ai <rajasekar012004@gmailcom>
2025-02-10 16:05:51 +0530

    created a python files

---
+++ calrect.py
@@ -0,0 +1,9 @@
+# calculate the area of a rectangle
+def calculate_area(length, width):
+    return length * width
+
+length = int(input("Enter the length of the rectangle: "))
+width = int(input("Enter the width of the rectangle: "))
+
+area = calculate_area(length, width)
+print(f"The area of the rectangle is: {area}")
