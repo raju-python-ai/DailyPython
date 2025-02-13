cc77f736ab4e3f77140ce1f6c7f1c93a307c1562
raju-python-ai <rajasekar012004@gmailcom>
2025-02-13 12:09:33 +0530

    created a python files

---
+++ minmax.py
@@ -0,0 +1,19 @@
+list=[2,3,4,5,6,7.4,6,8,2,34,12,2,0]
+min_value=min(list)
+max_value=max(list)
+print("minimum values",min_value)
+print("maximum values",max_value)
+
+# def find_min_max(list):
+#     min_val= max_val= list[0]
+#     for num in list[1:]:
+#         if num < min_val:
+#             min_val=num
+#         if num > max_val:
+#             max_val=num
+#     return min_val,max_val
+# numbers=[3,7,2,9,5,1,8] 
+# min_val, max_val =find_min_max(numbers)
+        
+# print("minimum values:",min_val)
+# print("maximum values:",max_val)
