f9e54f1916a67449db5e01fbfeb95271166067b4
raju-python-ai <rajasekar012004@gmailcom>
2025-03-06 21:32:32 +0530

    changed my python files

---
+++ primeloop.py
@@ -0,0 +1,8 @@
+for num in range(1, 51):  
+    if num > 1:  
+        for i in range(2, int(num ** 0.5) + 1):  
+            if num % i == 0:
+                break
+        else:
+            print(num)
+            
